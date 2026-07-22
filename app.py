e62c849ecd0468c417cf73f74ec288ae8f0e06c9
VMurtis <v.murtis@gmail.com>
2026-07-22 18:31:35 -0300

    adiciona tratamento de erro e validacao de opcao no menu

--- app.py
+++ app.py
@@ -20,19 +20,28 @@ def finalizar_app():
     os.system('cls')
     print('Finalizando o app')
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input("Digite uma tecla para voltar ao menu principal")
+    main()
+
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2: 
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3: 
-        print('Ativar restaurante')
-    else: 
-        finalizar_app()
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        else: 
+            finalizar_app()
+    except:
+        opcao_invalida()
 
+        
 def main():
     exibir_nome_do_programa()
     exibir_opcoes()
