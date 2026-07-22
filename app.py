26d67496e7271f55321fe98f08fdefe799cab3c1
VMurtis <v.murtis@gmail.com>
2026-07-22 18:13:52 -0300

    adiciona menu inicial do sistema Sabor Express

---
+++ app.py
@@ -0,0 +1,19 @@
+
+print("Sabor Express\n")
+
+print("1. Cadastar restaurante")
+print("2. listar restaurante")
+print("3. Ativar restaurante")
+print("4. sair \n")
+
+opcao_escolhida = int(input("Escolha um opção: "))
+print(f"você escolheu a opção: {opcao_escolhida}")
+
+if opcao_escolhida == 1: 
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2: 
+    print('Listar restaurantes')
+elif opcao_escolhida == 3: 
+    print('Ativar restaurante')
+else:
+    print("Finalizando o app")
